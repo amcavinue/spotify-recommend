cbffd948ae24ba3b38873a0c20dca940d9b40e02
Aaron <aaronmcavinue@gmail.com>
2016-11-21 18:56:36 +0000

    Add functionality for retrieving and showing related artists.

--- server.js
+++ server.js
@@ -8,12 +8,25 @@ var getFromApi = function(endpoint, args) {
            .qs(args)
            .end(function(response) {
                 if (response.ok) {
-                    emitter.emit('end', response.body);
+                    emitter.emit('end', response.body, response.code);
                 }
                 else {
                     emitter.emit('error', response.code);
                 }
             });
+    return emitter;
+};
+
+function getFromRelatedApi(id) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/artists/' + id + '/related-artists')
+        .end(function(response) {
+            if (response.ok) {
+                emitter.emit('end', response.body.artists);
+            } else {
+                emitter.emit('error', response.code);
+            }
+        }); 
     return emitter;
 };
 
@@ -27,9 +40,27 @@ app.get('/search/:name', function(req, res) {
         type: 'artist'
     });
 
-    searchReq.on('end', function(item) {
+    searchReq.on('end', function(item, code) {
+        if (item.artists.items.length === 0) {
+            res.sendStatus(404);
+            return;
+        }
+        
         var artist = item.artists.items[0];
-        res.json(artist);
+        var artists = [artist];
+        
+        var searchRelatedReq = getFromRelatedApi(artist.id);
+        
+        searchRelatedReq.on('end', function(related) {
+            console.log(related);
+            artists = artists.concat(related);
+            console.log(artists);
+            res.json(artists);
+        });
+        
+        searchRelatedReq.on('error', function(code) {
+            res.sendStatus(code);
+        });
     });
 
     searchReq.on('error', function(code) {
